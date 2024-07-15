8a88f5e211787605f7c06f9aa3a012f1dd1475c2
Brandon Nguyen <25nguyenb26@stu.smuhsd.org>
2024-07-14 21:46:24 -0700

    updating schema

---
+++ src/update-mongo.ts
@@ -0,0 +1,23 @@
+import { connect } from 'mongoose';
+import userSchema from './schema/user';
+import mongoConfig from './mongoConfig';
+
+const update = async () => {
+    try {
+        await connect(mongoConfig.MONGODB_URL);
+        const result = await userSchema.updateMany(
+            // Need to learn how to search for fields that don't exist
+            {},
+            {
+                $set: { gems: 0, daily_timestamp: new Date(0) },
+            },
+            { strict: false },
+        );
+
+        console.log(result);
+    } catch (err) {
+        console.log(err);
+    }
+};
+
+update();

29029df463380c33b8490fa9366f4c82ef8d50c9
Brandon Nguyen <brandonzn117@gmail.com>
2025-06-14 23:49:54 -0700

    Implementing Supabase for real

--- src/commands/inventory.ts
+++ src/commands/inventory.ts
@@ -1,18 +1,47 @@
 import { SlashCommandBuilder } from '@discordjs/builders';
-import { CommandInteraction } from 'discord.js';
+import { Client, CommandInteraction } from 'discord.js';
 
-import userSchema from '../schema/user';
 import DiscordCommand from './discord_command';
+import { supabase } from '..';
+import { getCard } from '../get_cards';
+
+export type Inventory = {
+    card_id: string;
+    quantity: number;
+}[];
 
 class InventoryCommand extends DiscordCommand {
-    data = new SlashCommandBuilder().setName('inventory').setDescription('See all your cards!');
+    constructor() {
+        super(new SlashCommandBuilder().setName('inventory').setDescription('See all your cards!'));
+    }
 
-    override async execute(interaction: CommandInteraction): Promise<void> {
-        const user = await userSchema.findOne({ uuid: interaction.user.id });
+    async fetchInventory(interaction: CommandInteraction): Promise<Inventory> {
+        const { data: inventoryData, error: inventoryError } = await supabase
+            .from('Inventory')
+            .select('card_id, quantity')
+            .eq('user_id', interaction.user.id);
 
-        await interaction.reply(
-            user.inventory.reduce((prev, current) => prev + current.name + ', ', ''),
-        );
+        if (inventoryError) {
+            console.error('Failed to fetch inventory:', inventoryError);
+            await interaction.reply('There was an error fetching your inventory.');
+            return;
+        }
+
+        return inventoryData;
+    }
+
+    textifyInventory(inventoryData: Inventory): string {
+        return inventoryData
+            .map((item) => `**${getCard(item.card_id).name}** x${item.quantity}`)
+            .join('\n');
+    }
+
+    override async execute(interaction: CommandInteraction, client: Client): Promise<void> {
+        // Build the inventory message
+        const data = await this.fetchInventory(interaction);
+        const inventoryText = this.textifyInventory(data);
+
+        await interaction.reply(inventoryText);
     }
 }
 
--- src/commands/remove_character.ts
+++ src/commands/remove_character.ts
@@ -1,32 +1,56 @@
 import { SlashCommandBuilder } from '@discordjs/builders';
-import { ChatInputCommandInteraction, CommandInteraction } from 'discord.js';
+import { ChatInputCommandInteraction, Client, CommandInteraction } from 'discord.js';
 
-import userSchema from '../schema/user';
 import DiscordCommand from './discord_command';
+import { supabase } from '..';
+import InventoryCommand from './inventory';
+import { getCard } from '../get_cards';
 
 class RemoveCommand extends DiscordCommand {
-    data = new SlashCommandBuilder()
-        .setName('remove_character')
-        .setDescription('Remove an unwanted character from your deck')
-        .addStringOption((option) => {
-            return option
-                .setName('character')
-                .setDescription('The character to delete')
-                .setRequired(true);
-        });
-
-    override async execute(interaction: ChatInputCommandInteraction): Promise<void> {
-        const updatedUser = await userSchema.findOneAndUpdate(
-            { uuid: interaction.user.id },
-            {
-                $pull: { inventory: interaction.options.getString('character') },
-            },
-            {
-                new: true,
-            },
+    constructor() {
+        super(
+            new SlashCommandBuilder()
+                .setName('remove_character')
+                .setDescription('Remove an unwanted character from your deck')
+                .addStringOption((option) => {
+                    return option
+                        .setName('character')
+                        .setDescription('The character to delete')
+                        .setRequired(true);
+                }),
         );
+    }
 
-        await interaction.reply(updatedUser.inventory.join(', '));
+    override async execute(
+        interaction: ChatInputCommandInteraction,
+        client: Client,
+    ): Promise<void> {
+        // const { data: card, error } = await supabase
+        //     .from('Cards')
+        //     .select('id, name')
+        //     .ilike('name', interaction.options.getString('character'))
+        //     .single();
+
+        const cardName = interaction.options.getString('character').toLowerCase();
+
+        const { error: deleteError } = await supabase
+            .from('Inventory')
+            .delete()
+            .eq('user_id', interaction.user.id)
+            .eq('card_id', getCard(cardName).id);
+
+        if (deleteError) {
+            console.error('Error deleting inventory item:', deleteError);
+            await interaction.reply(
+                `Failed to remove ${interaction.options.getString('character')} from your inventory.`,
+            );
+            return;
+        }
+
+        await interaction.reply('Successfully deleted **' + cardName + '**');
+
+        // Prints out updated inventory
+        await new InventoryCommand().execute(interaction, client);
     }
 }
 

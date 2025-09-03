c1c2d26bea107ed7f4dc46469c081d050ce6c1db
Brandon Nguyen <brandonzn117@gmail.com>
2025-09-03 16:29:31 -0700

    working evolutions

---
+++ src/commands/inventory/evolve.ts
@@ -0,0 +1,162 @@
+import { ActionRowBuilder, ButtonBuilder, SlashCommandBuilder } from '@discordjs/builders';
+import { ButtonStyle, ChatInputCommandInteraction, Message } from 'discord.js';
+
+import { supabase } from '../..';
+import InventoryBase from './inventory_base';
+import { Card, getCard } from '../../get_cards';
+
+class Evolve extends InventoryBase {
+    constructor() {
+        super(
+            new SlashCommandBuilder()
+                .setName('evolve')
+                .setDescription(
+                    'Evolve a character. You need 4 cards to evolve. View character IDs via /inventory_by_id',
+                )
+
+                .addIntegerOption((option) => {
+                    return option.setName('id').setDescription('4 digit id code').setRequired(true);
+                }),
+        );
+    }
+
+    override async execute(interaction: ChatInputCommandInteraction): Promise<void> {
+        await interaction.deferReply({ ephemeral: true }); // Acknowledge the interaction immediately
+
+        const cardNumber = interaction.options.getInteger('id');
+
+        const baseCard = getCard(cardNumber);
+
+        const { data: evoData, error: evoerror } = await supabase
+            .from('evolutions')
+            .select('evolution_card_id')
+            .eq('card_id', baseCard.id);
+
+        const evoCards = evoData.map((data) => getCard(data.evolution_card_id));
+
+        // Row 3: confirm (disabled at first)
+        const confirmRow = new ActionRowBuilder<ButtonBuilder>().addComponents(
+            new ButtonBuilder()
+                .setCustomId('cancel')
+                .setLabel('Cancel')
+                .setStyle(ButtonStyle.Danger),
+            new ButtonBuilder()
+                .setCustomId('confirm')
+                .setLabel('Confirm')
+                .setStyle(ButtonStyle.Success)
+                .setDisabled(evoCards.length !== 1),
+        );
+
+        let message: Message<boolean>;
+        let chosenEvo: Card;
+
+        let evoOptions: ActionRowBuilder<ButtonBuilder>;
+
+        if (evoCards.length === 1) {
+            message = await interaction.followUp({
+                content: `Are you sure that you want to evolve 4 **${baseCard.name}** into **${evoCards[0].name}**?`,
+                components: [confirmRow],
+                ephemeral: true,
+                fetchReply: true,
+            });
+
+            chosenEvo = evoCards[0];
+        } else {
+            evoOptions = new ActionRowBuilder<ButtonBuilder>().addComponents(
+                evoCards.map((card, idx) =>
+                    new ButtonBuilder()
+                        .setCustomId(`${idx}`)
+                        .setLabel(card.name)
+                        .setStyle(ButtonStyle.Secondary),
+                ),
+            );
+
+            message = await interaction.followUp({
+                content: `Choose an evolution path for **${baseCard.name}**`,
+                components: [evoOptions, confirmRow],
+                ephemeral: true,
+                fetchReply: true,
+            });
+        }
+
+        const collector = (message as Message).createMessageComponentCollector({
+            time: 5 * 60_000, // optional absolute max lifetime (5 minutes for safety)
+            idle: 60_000, // end if no interaction for 60s
+            filter: (i) => i.user.id === interaction.user.id,
+        });
+
+        collector.on('collect', async (i) => {
+            // Upload evo
+            if (i.customId === 'confirm') {
+                const { data, error } = await supabase.rpc('evolve_card', {
+                    p_card_id: baseCard.id,
+                    p_evo_card_id: chosenEvo.id,
+                    p_user_id: interaction.user.id,
+                });
+
+                if (error) {
+                    await i.update({
+                        content: `Evolution failed`,
+                        components: [],
+                    });
+
+                    console.log(error);
+                    return;
+                }
+
+                await i.update({
+                    content: `Evolution success! You got 1 ${chosenEvo.name}`,
+                    components: [],
+                });
+                collector.stop();
+                return;
+            } else if (i.customId === 'cancel') {
+                await i.update({
+                    content: `Evolution cancelled`,
+                    components: [],
+                });
+                collector.stop();
+                return;
+            } else {
+                const idx = parseInt(i.customId);
+                chosenEvo = evoCards[idx];
+
+                // Reset styles to Secondary, highlight the chosen one
+                evoOptions.components.forEach((btn, j) => {
+                    (btn as ButtonBuilder).setStyle(
+                        j === idx ? ButtonStyle.Primary : ButtonStyle.Secondary,
+                    );
+                });
+            }
+
+            // Update confirm button state
+            if (chosenEvo) {
+                // the confirm button
+                confirmRow.components[1].setDisabled(false);
+            }
+
+            // Update the message for move/target selection
+
+            if (evoCards.length === 1) {
+                await i.update({
+                    components: [confirmRow],
+                });
+            } else {
+                await i.update({
+                    components: [evoOptions, confirmRow],
+                });
+            }
+        });
+
+        collector.on('end', async (_collected, reason) => {
+            if (reason === 'time' && !chosenEvo) {
+                await message.edit({
+                    content: `⏳ Action selection timed out.`,
+                    components: [],
+                });
+            }
+        });
+    }
+}
+
+export default Evolve;
---
+++ src/commands/inventory/inventory_by_id.ts
@@ -0,0 +1,37 @@
+import { EmbedBuilder, SlashCommandBuilder } from '@discordjs/builders';
+import { Client, CommandInteraction } from 'discord.js';
+
+import InventoryBase from './inventory_base';
+import { fetchInventory } from './inventory_util';
+
+class CombatInventory extends InventoryBase {
+    constructor() {
+        super(
+            new SlashCommandBuilder()
+                .setName('inventory_by_id')
+                .setDescription('See all your cards, their IDs, and quantity'),
+        );
+    }
+
+    override async execute(interaction: CommandInteraction, client: Client): Promise<void> {
+        await interaction.deferReply({ ephemeral: false });
+        // Build the inventory message
+        const inventory = await fetchInventory(interaction);
+
+        const stringbuilt = inventory
+            .map((item) => {
+                return `${item.card.number}: **${item.card.name}** x${item.quantity}`;
+            })
+            .join('\n');
+
+        await interaction.editReply({
+            embeds: [
+                new EmbedBuilder()
+                    .setTitle(`${interaction.user.displayName}'s inventory by id`)
+                    .setDescription(stringbuilt),
+            ],
+        });
+    }
+}
+
+export default CombatInventory;
